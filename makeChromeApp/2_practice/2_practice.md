071a394c6c4175eb3c1b28f58a805fb98a6ecf33
KimYoonjae <rladbswo1124@gmail.com>
2020-06-22 01:23:09 +0900

    makeChromeApp 2.2 2.3 2.4

--- makeChromeApp/2_practice/2_practice.md
+++ makeChromeApp/2_practice/2_practice.md
@@ -24,3 +24,39 @@ function 함수이름(인자){
 
   
 
+## 2.2 JS DOM Functions
+
+- DOM: Document Object Module
+- getElementById
+
+
+
+## 2.3 Modifying the DOM with JS
+
+- querySelector
+
+
+
+## 2.4 Events and event handlers
+
+- addEventListener("상황", 함수명)
+  - "상황": "resize", "click", ...
+
+```javascript
+function 함수명(event){
+	console.log(event)
+}
+```
+
+```javascript
+const title = document.querySelector("#title");
+
+function handleClick(){
+    title.style.color = "blue";
+}
+
+title.addEventListener("click", handleClick);
+```
+
+
+
--- makeChromeApp/2_practice/index.js
+++ makeChromeApp/2_practice/index.js
@@ -1,23 +1,7 @@
-const calculator = {
-    plus: function(a, b){
-        return a + b;
-    },
-    minus: function(a, b){
-        return a - b;
-    },
-    multiply: function(a, b){
-        return a * b;
-    },
-    divide: function(a, b){
-        return a / b;
-    }
-};
+const title = document.querySelector("#title");
 
-// console.log(greetNicolas)
-const plus = calculator.plus(5, 5);
-const minus = calculator.minus(5, 5);
-const multiply = calculator.multiply(5, 5);
-const divide = calculator.divide(5, 5);
-console.log(plus);
-console.log(minus);
-console.log(`${multiply} ${divide}`)
+function handleClick(){
+    title.style.color = "blue";
+}
+
+title.addEventListener("click", handleClick);
